3c5a9508a7956625010ebd9efe508f17bd4198f7
VIKAS <VIKAS@LAPTOP-CQRN523G>
2022-10-07 18:32:31 +0530

    first commit

---
+++ src/VikS/TEST1.java
@@ -0,0 +1,19 @@
+package VikS;
+
+public class TEST1 {
+
+	public static void main(String[] args) {
+String name="vikas";
+		
+		if(name=="vikas")
+		{
+			System.out.println("test is pass");
+		}
+		else
+		{
+			System.out.println("test is fail");
+		}
+
+	}
+
+}
